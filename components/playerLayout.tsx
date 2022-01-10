bf248a2e1a0b6aba9ceb635236a445bf66880d89
wiztemple <wiztemple7@gmail.com>
2022-01-10 05:27:38 +0100

    player layout

---
+++ components/playerLayout.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/layout";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box as="main" width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        sidebar
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">{children}</Box>
+      <Box position="absolute" bottom="0" width="100%" left="0">
+        Player
+      </Box>
+    </Box>
+  );
+};
+
+export default PlayerLayout;
